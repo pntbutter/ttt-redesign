7de027ba68734d282557c178c6ccdac522d882b0
pntbutter <frallanawsm@gmail.com>
2019-11-29 00:28:06 +0100

    Hero slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
+// Icons
 const feather = require('feather-icons');
 feather.replace();
+
+
+// Slider
+import Swiper from 'swiper';
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 4000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  }
+});
